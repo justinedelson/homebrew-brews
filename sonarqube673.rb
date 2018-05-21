fedec2cbe8b95bbd4678d5d0e4e01c942966485f
Justin Edelson <justin@justinedelson.com>
2018-05-21 09:39:59 -0400

    adding sonar 673

---
+++ sonarqube673.rb
@@ -0,0 +1,48 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Sonarqube673 < Formula
+  desc ""
+  homepage ""
+  url "https://sonarsource.bintray.com/Distribution/sonarqube/sonarqube-6.7.3.zip"
+  sha256 "40e399714d5001366f61d6009bbbc4836789c832e9cb4d5cdddd2274e234c66f"
+  # depends_on "cmake" => :build
+
+  bottle :unneeded
+
+  depends_on :java => "1.8+"
+
+  def install
+    # Delete native bin directories for other systems
+    rm_rf Dir["bin/{linux,windows}-*"]
+
+    libexec.install Dir["*"]
+
+    bin.install_symlink "#{libexec}/bin/macosx-universal-64/sonar.sh" => "sonar"
+  end
+
+  plist_options :manual => "sonar console"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+        <string>#{opt_bin}/sonar</string>
+        <string>start</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+    </dict>
+    </plist>
+    EOS
+  end
+
+  test do
+    assert_match "SonarQube", shell_output("#{bin}/sonar status", 1)
+  end
+end
